22648a7cd58e35d92bf98b9c9fbbc85b483fa543
nirwan19 <31365060+nirwan19@users.noreply.github.com>
2018-09-30 11:56:52 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -106,25 +106,44 @@ if ($type == 'join' || $command == 'MENU') {
         'replyToken' => $replyToken,
         'messages' => array(
             array(
-                 "type": "flex",
-  		 "altText": "this is a flex message",
-  		 "contents": {
-    	 	 "type": "bubble",
-    		 "body": {
-      		 "type": "box",
-      		 "layout": "vertical",
-      		 "contents": [
-        			{
-          			"type": "text",
-          			"text": "hello"
-        			},
-        	{
-          	"type": "text",
-          	"text": "world"
-        	}
-      		]
-    		}
-  		}
+                 "type": "bubble",
+  "header": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Header text"
+      }
+    ]
+  },
+  "hero": {
+    "type": "image",
+    "url": "https://example.com/flex/images/image.jpg"
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Body text"
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Footer text"
+      }
+    ]
+  },
+  "styles": {
+    "comment": "See the example of a bubble style object"
+  }
             )
         )
     );
